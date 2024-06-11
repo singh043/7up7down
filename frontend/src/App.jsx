57fcacb376f1a7b249e1da48baf765dad8f99744
singh043 <kumarjatin431109@gmail.com>
2024-06-11 17:18:47 +0530

    final changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Dice from './components/dice/Dice';
 import { Button } from '@mui/material';
 import GuessNumber from './components/guessNumber/guessNumber';
 import Alert from '@mui/material/Alert';
+import CircularProgress from '@mui/material/CircularProgress';
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
 
@@ -16,10 +17,11 @@ function App() {
   const [guessedInput, setGuessedInput] = useState("");
   const [diceNumber1, setDiceNumber1] = useState(null);
   const [diceNumber2, setDiceNumber2] = useState(null);
+  const [loading, setLoading] = useState(false);
   
   const generateDiceNumber = async () => {
     try {
-      console.clear();
+      setLoading(true)
       const res = await axios.get('/api/diceNumber')
       setDiceNumber1(res.data.dice1)
       setDiceNumber2(res.data.dice2)
@@ -27,6 +29,7 @@ function App() {
         checkResult(res?.data?.dice1, res?.data?.dice2, guessedInput);
       }, 500)
     } catch (error) {
+      setLoading(false)
       console.log("Error", error?.message);
     }
   }
@@ -35,8 +38,9 @@ function App() {
     try {
       const res = await axios.post('/api/checkGuess', { diceNumber1, diceNumber2, guessedInput });
       setGuessResult(res?.data);
-      pointsCalculation(points, bet, guessedInput, guessResult)
+      pointsCalculation(points, bet, guessedInput, res?.data)
     } catch (error) {
+      setLoading(false)
       console.log("Error", error?.message)
     }
   }
@@ -45,7 +49,9 @@ function App() {
     try {
       const res = await axios.post('/api/calculatePoints', { totalPoints: points, betAmount: bet, guessResult, guessedInput });
       setPoints(res.data)
+      setLoading(false)
     } catch (error) {
+      setLoading(false)
       console.log("Error", error?.message);
     }
   }
@@ -73,10 +79,13 @@ function App() {
       <Button variant="contained"
         onClick={() => {
           generateDiceNumber();
+          setGuessResult("")
         }}
-        disabled={!bet || guessedInput === "" || points === 0}
+        disabled={!bet || guessedInput === "" || points === 0 || loading}
+        className={`${loading ? "loading" : ""} roll-btn`}
       >
-        Roll Die
+        {loading ? <CircularProgress size={16} /> : null}
+        <span>Roll Die</span>
       </Button>
       <div className='bet'>
         <Button variant="contained"
@@ -98,18 +107,24 @@ function App() {
           500
         </Button>
       </div>
-      {
-        guessResult ? 
-          guessResult === "You won" ?
-            <Alert severity="success" icon={<SentimentVerySatisfiedIcon />}>{`${guessResult}. Try luck again?`}</Alert>
-            :
-            <Alert severity="success" icon={<SentimentVeryDissatisfiedIcon />}>{`${guessResult}. Try again?`}</Alert>
+      <div className='result-text'>
+        {
+          loading ? 
+            <span><CircularProgress size={16} sx={{color: "white"}} />Loading ...</span>
           :
-          points === 0 ?
-            <Alert severity="success" icon={<SentimentVeryDissatisfiedIcon />}>You dont have enough points. Come back after 24 hours to get free 100 points.</Alert>
-            :
-            <Alert severity="success" icon={<false />}>Roll the dice to play the game</Alert>
-      }
+            points === 0 ?
+              <span><SentimentVeryDissatisfiedIcon />You do not have enough points. Come back after 24 hour to get free 100 points.</span>
+              :
+              guessResult ?
+              guessResult === "You won" ?
+                <span><SentimentVerySatisfiedIcon />You won. Try your luck again?</span>
+              :
+                <span><SentimentVeryDissatisfiedIcon />You lost. Try again?</span>
+              :
+                <span>Roll the dice to play the game.</span>
+
+        }
+      </div>
     </div>
   )
 }
